05e821ae59070c6bbf1c7a650d691e67f9824124
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-01-16 15:31:06 -0800

    Initial cleanup and structuring

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import '../styles/home.sass';
+function Home() {
+  return (
+    <>
+      <div className='HomeBody'>
+        <h1>All Creative Creations</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
