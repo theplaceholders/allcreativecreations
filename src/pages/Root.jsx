8df5faf1ff13bf366a32eb7fd2b59dc1a2aabacf
Old-Lai <89488963+Old-Lai@users.noreply.github.com>
2024-01-19 11:08:07 -0800

    Revert "Vite config"

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
-import { NavBar } from "../components";
 
 const Root = () => {
   return (
     <>
         <header>
-          <NavBar />
+
         </header>
         <main>
             <Outlet context={{}}/>
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host: true
-  },
   plugins: [react()],
 })
