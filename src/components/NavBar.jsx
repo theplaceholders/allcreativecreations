0e9ff99426b77b57b69396479926dc9e3392a197
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-01-19 11:04:17 -0800

    added menuIcon with animation, and included logo on navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,18 @@
+import MenuIcon from "./MenuIcon"
+import logo from '../assets/logo_without_text.png'
+import '../styles/components_styles/navBar.sass'
 const NavBar = () => {
+    const handleClickEvent = (e) => {
+        console.log("MenuIcon was clicked")
+        e.currentTarget.classList.toggle("close")
+        console.log(e.currentTarget)
+    }
     return(
         <nav>
-            <p>This is a nav bar</p>
+            <MenuIcon handleClickEvent={handleClickEvent}/>
+            <div className="navbar__logo">
+                <img src={logo} alt="logo" />
+            </div>
         </nav>
     )
 }
