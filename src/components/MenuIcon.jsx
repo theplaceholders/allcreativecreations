d4d0748ef7c94aa52d0fc3dff1328e4d414225a2
Old-Lai <89488963+Old-Lai@users.noreply.github.com>
2024-01-19 11:15:35 -0800

    Revert "Revert "Vite config""

---
+++ src/components/MenuIcon.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import '../styles/components_styles/menuIcon.sass';
+
+const MenuIcon = ({handleClickEvent}) => {
+    return(
+        <button className="menu-icon" onClick={(e)=>handleClickEvent(e)}>
+            <div className="menu-icon__bar"/>
+            <div className="menu-icon__bar"/>
+            <div className="menu-icon__bar"/>
+        </button>
+    )
+}
+
+MenuIcon.propTypes = {
+    handleClickEvent: PropTypes.func.isRequired
+};
+
+export default MenuIcon;
