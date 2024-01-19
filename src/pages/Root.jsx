72adcf94da4a63e37f01e2d52ead22756c5edfed
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-01-19 15:23:54 -0800

    fixed indentations

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,20 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../components";
 
+import "../styles/pages_styles/root.sass";
+
 const Root = () => {
   return (
     <>
-        <header>
-          <NavBar />
-        </header>
-        <main>
-            <Outlet context={{}}/>
-        </main>
-        <footer>
-
-        </footer>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Outlet context={{}} />
+      </main>
+      <footer></footer>
     </>
   );
-}
+};
 
 export default Root;
