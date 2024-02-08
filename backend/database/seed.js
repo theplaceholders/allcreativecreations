ab01ad90984143bb0dd7c58f37459dc245746469
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-02-07 17:36:47 -0800

    database function + initial data
    add basic database functions
    add initial data for admin user
    add initial customers

--- backend/database/seed.js
+++ backend/database/seed.js
@@ -1,5 +1,7 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 const pool = require('../Client');
+const { createUser, createCustomer, createReservation } = require('./db');
 
 
 async function dropTables() {
@@ -9,6 +11,7 @@ async function dropTables() {
     await pool.query(`
       DROP TABLE IF EXISTS reservations;
       DROP TABLE IF EXISTS customers;
+      DROP TABLE IF EXISTS users;
     `);
 
     console.log('Finished dropping tables!');
@@ -23,6 +26,13 @@ async function createTables() {
     console.log('Starting to build tables...');
 
     await pool.query(`
+        CREATE TABLE users(
+            internal_ID UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+            username VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            role VARCHAR(10) DEFAULT 'user'
+        );
         CREATE TABLE customers(
             internal_ID UUID DEFAULT gen_random_uuid() PRIMARY KEY,
             customer_ID INTEGER UNIQUE NOT NULL,
@@ -52,15 +62,28 @@ async function insertInitialData() {
   try {
     console.log('Starting to insert initial data...');
 
-    // const hashedPassword = await bcrypt.hash('admin123', 10);
+    //+process.env.SALT_ROUNDS << the + before process.env.SALT_ROUNDS is to convert the string to a number
+    const hashedPassword = await bcrypt.hash('admin', +process.env.SALT_ROUNDS);
 
-    // await createUser({ username:"admin", password:hashedPassword, email:"admin@example.com", isAdmin:true})
+    await createUser({ username:"admin", password:hashedPassword, email:"admin@example.com", role:"admin"})
+    .then((result) => {console.log(result)})
 
-    // const pokemonData = await getPokemonData();
+    const customers = [
+      { customer_ID: 1, first_name: 'John', last_name: 'Doe', email: 'test@example.com' },
+      { customer_ID: 2, first_name: 'John1', last_name: 'Doe1', email: 'test1@example.com' },
+      { customer_ID: 3, first_name: 'John2', last_name: 'Doe2', email: 'test2@example.com' },
+      { customer_ID: 4, first_name: 'John3', last_name: 'Doe3', email: 'test3@example.com' },
+      { customer_ID: 5, first_name: 'John4', last_name: 'Doe4', email: 'test4@example.com' },
+      { customer_ID: 6, first_name: 'John5', last_name: 'Doe5', email: 'test5@example.com' },
+      { customer_ID: 7, first_name: 'John6', last_name: 'Doe6', email: 'test6@example.com' },
+      { customer_ID: 8, first_name: 'John7', last_name: 'Doe7', email: 'test7@example.com' },
+      { customer_ID: 9, first_name: 'John8', last_name: 'Doe8', email: 'test8@example.com' },
+    ]
 
-
-    // await createProducts(pokemonData);
-
+    customers.map(async (customer) => {
+      await createCustomer(customer)
+      .then((result) => {console.log(result)})
+    })
     
     console.log('Finished inserting initial data!');
   } catch (e) {
