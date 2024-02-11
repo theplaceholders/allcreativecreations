9294cb375438eba4cc5beaf02eb135cdab414aad
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-02-10 22:12:35 -0800

    add react-calendar library
    added a library for calendar display
    planning to show reserved dates and show certain information in admin panel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 import '../styles/pages_styles/home.sass';
+
 function Home() {
+  const [value, onChange] = useState(new Date());
   return (
     <div className='HomeBody'>
       <h1>All Creative Creations</h1>
@@ -21,6 +26,7 @@ function Home() {
       <h1>All Creative Creations</h1>
       <h1>All Creative Creations</h1>
       <h1>All Creative Creations</h1>
+      <Calendar onChange={onChange} value={value} />
     </div>
   );
 }
